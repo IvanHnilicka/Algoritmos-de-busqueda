aceb6546be19f7a8a0c20fdcc7ae295ddb1f2c9f
Ivan Hnilicka <ivan.hnilicka21@outlook.com>
2023-04-11 20:32:39 -0600

    Se agregaron comentarios a algoritmo Local Beam Search, asi como las imagenes de sus respectivos grafos de ejemplo

--- local_beam_search.py
+++ local_beam_search.py
@@ -1,4 +1,5 @@
 def lbs(nodo_inicial, nodo_objetivo, grafo, heuristicas, beam):
+#   Almacena todos los posibles caminos que se pueden visitar
     nodos = []
     frontera.append(nodo_inicial)
 
@@ -8,27 +9,34 @@ def lbs(nodo_inicial, nodo_objetivo, grafo, heuristicas, beam):
     frontera.clear()
     explorados.append(nodo_inicial)
 
+#   Agregamos los adyacentes del nodo inicial a la lista de posibles caminos
     for adyacente in grafo[nodo_inicial]:
         nodos.append(adyacente)
 
+#   El algoritmo continua hasta haber explorado el nodo objetivo
     while not explorados.count(nodo_objetivo):
         contador = len(nodos)
 
+#       Si tenemos el mismo numero de caminos disponibles que el limite los agregamos directamente
         if contador == beam:
             for nodo in nodos:
                 frontera.append(nodo)
             nodos.clear()
+#       Si no eliminamos aquellos de mayor costo hasta tener solo los que cumplan con el limite
         else:
             while contador > beam:
                 nodo_mayor_costo = nodos[0]
 
+#               Buscamos el nodo de mayor costo de entre todos los disponibles
                 for nodo in nodos:
                     if heuristicas[nodo] > heuristicas[nodo_mayor_costo]:
                         nodo_mayor_costo = nodo
 
+#               Lo eliminamos y reducimos el contador en 1 debido a que ya tenemos 1 camino dispoible menos
                 nodos.pop(nodos.index(nodo_mayor_costo))
                 contador -= 1
 
+#           Agregamos los nodos que quedaron a la frontera
             for nodo in nodos:
                 frontera.append(nodo)
             nodos.clear()
@@ -37,13 +45,14 @@ def lbs(nodo_inicial, nodo_objetivo, grafo, heuristicas, beam):
         print("Frontera: ", frontera)        
         print("Explorados: ", explorados, "\n")
 
-
+#       Pasamos los nodos de la frontera a los explorados
         for nodo in frontera:
             explorados.append(nodo)
-
+#           Agregamos los adyacentes de los nodos en la frontera debido a que ya se visitaron
             for adyacente in grafo[nodo]:
                 nodos.append(adyacente)
 
+#       Reiniciamos la frontera para tener solo los nuevos nodos de la siguiente iteracion
         frontera.clear()
 
     print("Frontera: ", frontera)
@@ -52,7 +61,7 @@ def lbs(nodo_inicial, nodo_objetivo, grafo, heuristicas, beam):
 
 
 #   Nodo: [ adyacentes ]
-grafo1 = {
+LocalBeamSearch1 = {
     'S': ['A', 'B'],
     'A': ['C','D'],
     'B': ['E', 'F'],
@@ -72,7 +81,7 @@ grafo1 = {
 
 #   Nodo: heuristica
 heuristica1 = {
-    'S': 10,
+    'S': 12,
     'A': 7,
     'B': 10,
     'C': 5,
@@ -91,7 +100,7 @@ heuristica1 = {
 
 
 
-grafo2 = {
+LocalBeamSearch2 = {
     'A': ['B', 'C', 'D'],
     'B': ['G', 'H'],
     'C': [],
@@ -127,4 +136,5 @@ heuristica2 = {
 frontera = []
 explorados = []
 
-lbs('A', 'J', grafo2, heuristica2, 3)
+lbs('S', 'L', LocalBeamSearch1, heuristica1, 2)
+# lbs('A', 'J', LocalBeamSearch2, heuristica2, 3)
